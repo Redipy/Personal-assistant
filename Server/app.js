ac08dcaa7b647706b5bf39c4e910f60c70d5fe2e
Redipy59 <1076238951@qq.com>
2019-11-26 20:34:50 +0800

    11.

--- Server/app.js
+++ Server/app.js
@@ -13,14 +13,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
-});
+var allowCors = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials','true');
+  next();
+};
+  
+app.use(allowCors);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
